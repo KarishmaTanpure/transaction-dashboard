10a7cf6477d5dad64a845f5f55d65675a0721e66
Karishma Tanpure <karishmaptanpure95@gmail.com>
2025-02-06 14:29:12 +0530

    Initial commit

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-    DB_URI: 'mongodb+srv://karishmaptanpure95:knit123@cluster0.jajlf.mongodb.net/transactionsDB?retryWrites=true&w=majority',
+    DB_URI: 'mongodb+srv://karishmaptanpure95:knit123@cluster0.308tk.mongodb.net/transactionsDB?retryWrites=true&w=majority&appName=Cluster0',
     THIRD_PARTY_API_URL: 'https://s3.amazonaws.com/roxiler.com/product_transaction.json',
 };
